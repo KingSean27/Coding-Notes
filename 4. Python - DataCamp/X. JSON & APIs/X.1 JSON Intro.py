48f1a988e243d58a03397edde5e7ab65b28021f4
Sean LC <sean.cornu.16@ucl.ac.uk>
2022-05-07 11:50:57 +0100

    Data Camp - Additions

--- 4. Python - DataCamp/X. JSON & APIs/X.1 JSON Intro.py
+++ 4. Python - DataCamp/X. JSON & APIs/X.1 JSON Intro.py
@@ -1,12 +1,12 @@
 # Javascript object notation (JSON) is a data type originally created with Java but is now widely used
 # it is easy for machines and humans alike, especially on the web
 # JSON data is not tabular, records don't have to all have the same set of attributes
-# they are similar to python dictionaries
+# they are similar to python dictionaries and can be nested
 
 # we can use read_json to take in a json - use it to find the string of the data to load
 
 import pandas as pd
 
-#a = pd.read_json ()
+# a = pd.read_json ()
 
-
+# data types are guessed but can be specified with the dtype keyword argument
